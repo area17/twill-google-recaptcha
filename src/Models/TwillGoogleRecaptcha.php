2a1dd12be2fad289c290ac876b18fac72e81172b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2022-11-16 17:16:26 +0100

    Encrypt database content

--- src/Models/TwillGoogleRecaptcha.php
+++ src/Models/TwillGoogleRecaptcha.php
@@ -3,6 +3,8 @@
 namespace A17\TwillGoogleRecaptcha\Models;
 
 use A17\Twill\Models\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Crypt;
 use A17\Twill\Models\Behaviors\HasRevisions;
 
 class TwillGoogleRecaptcha extends Model
@@ -13,16 +15,42 @@ class TwillGoogleRecaptcha extends Model
 
     public function getSiteKeyAttribute(): string|null
     {
-        return google_recaptcha()->siteKey(true);
+        return $this->decrypt(google_recaptcha()->siteKey(true));
+    }
+
+    public function setSiteKeyAttribute($value): void
+    {
+        $this->attributes['site_key'] = $this->encrypt($value);
     }
 
     public function getPrivateKeyAttribute(): string|null
     {
-        return google_recaptcha()->privateKey(true);
+        return $this->decrypt(google_recaptcha()->privateKey(true));
+    }
+
+    public function setPrivateKeyAttribute($value): void
+    {
+        $this->attributes['private_key'] = $this->encrypt($value);
     }
 
     public function getPublishedAttribute(): string|null
     {
         return google_recaptcha()->published(true);
+    }
+
+    public function encrypt($value)
+    {
+        return Crypt::encryptString($value);
+    }
+
+    public function decrypt($value)
+    {
+        try {
+            $decrypted = Crypt::decryptString($value);
+        } catch (\Throwable) {
+            $decrypted = $value;
+        }
+
+        return $decrypted;
     }
 }
